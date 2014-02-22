12aabc04d69ffecb2414a7d46cead69893cb85a9
Martin Boyanov <mboyanov@gmail.com>
2014-02-22 15:15:15 +0000

    Initial

---
+++ circle.py
@@ -0,0 +1,8 @@
+__author__ = 'marty'
+
+class circle:
+    def __init__(self,x,y,radius,name):
+        self.x=x
+        self.y=y
+        self.radius=radius
+        self.name=name
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='try',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='marty',
+    author_email='',
+    description=''
+)
